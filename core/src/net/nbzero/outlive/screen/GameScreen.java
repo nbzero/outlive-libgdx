74fcba05bec6b41d0881f955623631353ed7e275
Purim Nimsaeng <nbzero0@gmail.com>
2016-11-12 22:38:34 +0700

    Add game HUD

--- core/src/net/nbzero/outlive/screen/GameScreen.java
+++ core/src/net/nbzero/outlive/screen/GameScreen.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 import net.nbzero.outlive.InputsControl;
+import net.nbzero.outlive.hud.GameScreenHUD;
 import net.nbzero.outlive.player.PlayerData;
 import net.nbzero.outlive.player.characters.Character;
 import net.nbzero.outlive.player.characters.CharacterFactory;
@@ -19,10 +23,12 @@ import net.nbzero.outlive.player.characters.Fireball;
 import net.nbzero.outlive.player.characters.Stone;
 import net.nbzero.outlive.positon.PositionHandler;
 import net.nbzero.outlive.utils.CollideHandler;
+import net.nbzero.outlive.utils.Utils;
 
 public class GameScreen implements Screen {
 	public static SpriteBatch batch;
-	protected static float elapsedTime;
+	private static Stage stage;
+	public static float elapsedTime;
 	private static PlayerData p1;
 	private static PlayerData p2;
 	private static Texture bg;
@@ -38,12 +44,21 @@ public class GameScreen implements Screen {
 	protected static String p2Char = "Usopp";
 	private static String bgPath = "Stage/forest.png";
 	protected static ArrayList<Fireball> fireballs; 
+	protected static int checkFireball=1;
+	private static Label player1Label;
+	private static Label player2Label;
+	private static Label player1HPLabel;
+	private static Label player2HPLabel;
+	private static Label player1MPLabel;
+	private static Label player2MPLabel;
+	private static Label timerLabel;
 	protected static ArrayList<Stone> stones; 
 	
 	@Override
 	public void show() {
 		debugMode = true;
 		initialize();
+		initialHUD();
 	}
 
 	@Override
@@ -141,6 +156,7 @@ public class GameScreen implements Screen {
 			player1.getPlayer().setDelayTime(GameScreenAtkUtils.getSkill2Time(player1, player1.getPlayer().getDelayTime()));
 		}
 		else if(player1.getPlayer().isDead()){
+			player1.getPlayer().setHp(0);
 			player1.getPlayer().setDeadTime(player1.getPlayer().getDeadTime()+Gdx.graphics.getDeltaTime());
 			GameScreenDrawAnim.deadAnim(player1);
 		}
@@ -149,6 +165,11 @@ public class GameScreen implements Screen {
 		}
 		else {
 			GameScreenDrawAnim.idleAnim(player1);
+			if(player1.getPlayer().getHp()<=0){
+				player1.getPlayer().setHp(0);
+				player1.getPlayer().setDead(true);
+				player1.getPlayer().setHasControl(false);
+			}
 		}
 		
 		//// Player2
@@ -231,6 +252,7 @@ public class GameScreen implements Screen {
 			player2.getPlayer().setDelayTime(GameScreenAtkUtils.getSkill2Time(player2, player2.getPlayer().getDelayTime()));
 		}
 		else if(player2.getPlayer().isDead()){
+			player2.getPlayer().setHp(0);
 			player2.getPlayer().setDeadTime(player2.getPlayer().getDeadTime()+Gdx.graphics.getDeltaTime());
 			GameScreenDrawAnim.deadAnim(player2);
 		}
@@ -239,6 +261,11 @@ public class GameScreen implements Screen {
 		}
 		else {
 			GameScreenDrawAnim.idleAnim(player2);
+			if(player2.getPlayer().getHp()<=0){
+				player2.getPlayer().setHp(0);
+				player2.getPlayer().setDead(true);
+				player2.getPlayer().setHasControl(false);
+			}
 		}
 		//update fireball
 		ArrayList<Fireball> fireballsToRemove = new ArrayList<Fireball>();
@@ -271,7 +298,12 @@ public class GameScreen implements Screen {
 		
 		batch.end();
 		
+		renderHUD();
 		renderDebugMode();
+		System.out.println(player1.getPlayer().getHp());
+		
+		stage.act(delta);
+		stage.draw();
 	}
 
 	@Override
@@ -308,10 +340,88 @@ public class GameScreen implements Screen {
 		player2 = CharacterFactory.valueOf(CharacterSelectScreen.p2Char).getNew(p2);
 		bg = new Texture(bgPath);
 		batch = new SpriteBatch();
+		stage = new Stage();
 		shapeRenderer = new ShapeRenderer();
 		elapsedTime = 0;
 		fireballs = new ArrayList<Fireball>();
+		GameScreenHUD.load();
 		stones = new ArrayList<Stone>();
+	}
+	
+	private void renderHUD(){
+		Gdx.gl.glEnable(GL20.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		shapeRenderer.begin(ShapeType.Filled);
+		// BG
+		shapeRenderer.setColor(new Color(1, 1, 1, 0.75f));
+		shapeRenderer.rect(GameScreenHUD.getP1PlayerBarBG1().getX(), GameScreenHUD.getP1PlayerBarBG1().getY(),
+				GameScreenHUD.getP1PlayerBarBG1().getWidth(), GameScreenHUD.getP1PlayerBarBG1().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP1PlayerBarBG2().getX(), GameScreenHUD.getP1PlayerBarBG2().getY(),
+				GameScreenHUD.getP1PlayerBarBG2().getWidth(), GameScreenHUD.getP1PlayerBarBG2().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP2PlayerBarBG1().getX(), GameScreenHUD.getP2PlayerBarBG1().getY(),
+				GameScreenHUD.getP2PlayerBarBG1().getWidth(), GameScreenHUD.getP2PlayerBarBG1().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP2PlayerBarBG2().getX(), GameScreenHUD.getP2PlayerBarBG2().getY(),
+				GameScreenHUD.getP2PlayerBarBG2().getWidth(), GameScreenHUD.getP2PlayerBarBG2().getHeight());
+		// Base HP, MP
+		shapeRenderer.setColor(0, 0, 0, 1);
+		shapeRenderer.rect(GameScreenHUD.getP1BaseHPBar().getX(), GameScreenHUD.getP1BaseHPBar().getY(),
+				GameScreenHUD.getP1BaseHPBar().getWidth(), GameScreenHUD.getP1BaseHPBar().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP1BaseMPBar().getX(), GameScreenHUD.getP1BaseMPBar().getY(),
+				GameScreenHUD.getP1BaseMPBar().getWidth(), GameScreenHUD.getP1BaseMPBar().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP2BaseHPBar().getX(), GameScreenHUD.getP2BaseHPBar().getY(),
+				GameScreenHUD.getP2BaseHPBar().getWidth(), GameScreenHUD.getP2BaseHPBar().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP2BaseMPBar().getX(), GameScreenHUD.getP2BaseMPBar().getY(),
+				GameScreenHUD.getP2BaseMPBar().getWidth(), GameScreenHUD.getP2BaseMPBar().getHeight());
+		// Dynamic HP, MP
+		shapeRenderer.setColor(0.54f, 0.71f, 0.18f, 1);
+		shapeRenderer.rect(GameScreenHUD.getP1BaseHPBar().getX(), GameScreenHUD.getP1BaseHPBar().getY(),
+				player1.getPlayer().getHPPercentage()*250, GameScreenHUD.getP1BaseHPBar().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP2BaseHPBar().getX(), GameScreenHUD.getP2BaseHPBar().getY(),
+				player2.getPlayer().getHPPercentage()*250, GameScreenHUD.getP2BaseHPBar().getHeight());
+		shapeRenderer.setColor(0.22f, 0.63f, 0.65f, 1);
+		shapeRenderer.rect(GameScreenHUD.getP1BaseMPBar().getX(), GameScreenHUD.getP1BaseMPBar().getY(),
+				player1.getPlayer().getMPPercentage()*250, GameScreenHUD.getP1BaseMPBar().getHeight());
+		shapeRenderer.rect(GameScreenHUD.getP2BaseMPBar().getX(), GameScreenHUD.getP2BaseMPBar().getY(),
+				player2.getPlayer().getMPPercentage()*250, GameScreenHUD.getP2BaseMPBar().getHeight());
+		shapeRenderer.end();
+		Gdx.gl.glDisable(GL20.GL_BLEND);
+		player1HPLabel.setText((int) player1.getPlayer().getHp()+"/"+(int) player1.getPlayer().getMaxHP());
+		player1MPLabel.setText((int) player1.getPlayer().getMp()+"/"+(int) player1.getPlayer().getMaxMP());
+		player2HPLabel.setText((int) player2.getPlayer().getHp()+"/"+(int) player2.getPlayer().getMaxHP());
+		player2MPLabel.setText((int) player2.getPlayer().getMp()+"/"+(int) player2.getPlayer().getMaxMP());
+		timerLabel.setText(String.valueOf((int) elapsedTime));
+	}
+	
+	private void initialHUD(){
+		player1Label = new Label(CharacterSelectScreen.p1Char + " (P1)", Utils.gameScreenSkin, "imagineFontPlayer", Color.BLACK);
+		player2Label = new Label(CharacterSelectScreen.p2Char + " (P2)", Utils.gameScreenSkin, "imagineFontPlayer", Color.BLACK);
+		player1Label.setPosition(GameScreenHUD.getP1NameLabel().getX(), GameScreenHUD.getP1NameLabel().getY());
+		player2Label.setPosition(GameScreenHUD.getP2NameLabel().getX(), GameScreenHUD.getP2NameLabel().getY());
+		player1HPLabel = new Label((int) player1.getPlayer().getHp()+"/"+(int) player1.getPlayer().getMaxHP(), Utils.gameScreenSkin, "imagineFontHpMp", Color.WHITE);
+		player1MPLabel = new Label((int) player1.getPlayer().getMp()+"/"+(int) player1.getPlayer().getMaxMP(), Utils.gameScreenSkin, "imagineFontHpMp", Color.WHITE);
+		player2HPLabel = new Label((int) player2.getPlayer().getHp()+"/"+(int) player2.getPlayer().getMaxHP(), Utils.gameScreenSkin, "imagineFontHpMp", Color.WHITE);
+		player2MPLabel = new Label((int) player2.getPlayer().getMp()+"/"+(int) player2.getPlayer().getMaxMP(), Utils.gameScreenSkin, "imagineFontHpMp", Color.WHITE);
+		player1HPLabel.setPosition(GameScreenHUD.getP1HPLabel().getX(), GameScreenHUD.getP1HPLabel().getY());
+		player1MPLabel.setPosition(GameScreenHUD.getP1MPLabel().getX(), GameScreenHUD.getP1MPLabel().getY());
+		player2HPLabel.setPosition(GameScreenHUD.getP2HPLabel().getX(), GameScreenHUD.getP2HPLabel().getY());
+		player2MPLabel.setPosition(GameScreenHUD.getP2MPLabel().getX(), GameScreenHUD.getP2MPLabel().getY());
+		timerLabel = new Label(String.valueOf((int) elapsedTime), Utils.gameScreenSkin, "imagineFontTimer", Color.WHITE);
+		timerLabel.setPosition(GameScreenHUD.getTimerLabel().getX(), GameScreenHUD.getTimerLabel().getY());
+		
+		stage.addActor(player1Label);
+		stage.addActor(player2Label);
+		stage.addActor(player1HPLabel);
+		stage.addActor(player1MPLabel);
+		stage.addActor(player2HPLabel);
+		stage.addActor(player2MPLabel);
+		stage.addActor(timerLabel);
+	}
+	
+	private void pauseScreen(){
+		Gdx.gl.glEnable(GL20.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		shapeRenderer.begin(ShapeType.Filled);
+		Gdx.gl.glDisable(GL20.GL_BLEND);
 	}
 	
 	private void renderDebugMode(){
